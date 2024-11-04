0d53d151e668bb9839c9cbf32a0626da5ccda44c
Viqtor <viqto0r@gmail.com>
2024-11-04 17:56:07 +0300

    Вывод погоды

--- services/log.service.js
+++ services/log.service.js
@@ -1,7 +1,7 @@
 import chalk from 'chalk'
 import dedent from 'dedent-js'
 
-const { bgRed, bgGreen, bgCyan } = chalk
+const { bgRed, bgGreen, bgCyan, bgYellow } = chalk
 
 export const printError = (error) => {
   console.log(`${bgRed(' ERROR ')} ${error}`)
@@ -18,5 +18,22 @@ export const printHelp = () => {
     -s [CITY] - для установки города
     -h для вывода помощи
     -t [API_KEY] - для сохранения токена`
+  )
+}
+
+export const printWeather = (res, icon) => {
+  const city = res.name
+  const temp = Math.round(res.main.temp)
+  const feelsLike = Math.round(res.main.feels_like)
+  const description = res.weather[0].description
+  const humidity = res.main.humidity
+  const windSpeed = res.wind.speed
+
+  console.log(
+    dedent`${bgYellow(' WEATHER ')} Погода в городе ${city}:
+    ${icon}  ${description}
+    Температура: ${temp}° (ощущается как ${feelsLike}°)
+    Влажность: ${humidity}%
+    Скорость ветра: ${windSpeed}м/c`
   )
 }
--- weather.js
+++ weather.js
@@ -1,10 +1,24 @@
 #!/usr/bin/env node
+import axios from 'axios'
 import { getArgs } from './helpers/args.js'
-import { getWeather } from './services/api.service.js'
-import { printError, printHelp, printSuccess } from './services/log.service.js'
-import { saveKeyValue, TOKEN_DICTIONARY } from './services/storage.service.js'
+import { getIcon, getWeather } from './services/api.service.js'
+import {
+  printError,
+  printWeather,
+  printHelp,
+  printSuccess,
+} from './services/log.service.js'
+import {
+  getValueByKey,
+  saveKeyValue,
+  TOKEN_DICTIONARY,
+} from './services/storage.service.js'
 
 const saveToken = async (token) => {
+  if (!token.length) {
+    return printError('Не передан токен')
+  }
+
   try {
     await saveKeyValue(TOKEN_DICTIONARY.token, token)
     printSuccess('Токен сохранён')
@@ -13,22 +27,63 @@ const saveToken = async (token) => {
   }
 }
 
+const saveCity = async (city) => {
+  if (!city.length) {
+    return printError('Не передан город')
+  }
+
+  try {
+    await saveKeyValue(TOKEN_DICTIONARY.city, city)
+    printSuccess('Город сохранён')
+  } catch (e) {
+    printError(e.message)
+  }
+}
+
+const getForecast = async () => {
+  try {
+    const city =
+      process.env.CITY ?? (await getValueByKey(TOKEN_DICTIONARY.city))
+    const weatherData = await getWeather(city)
+    const icon = weatherData.weather[0].icon
+
+    printWeather(weatherData, getIcon(icon))
+  } catch (e) {
+    const status = e?.response?.status
+    let errorMessage = e.message
+
+    switch (status) {
+      case 404:
+        errorMessage = 'Неверно указан город'
+        break
+      case 401:
+        errorMessage = 'Неверно указан токен'
+        break
+    }
+
+    printError(errorMessage)
+  }
+}
+
 const initCLI = async () => {
   const args = getArgs(process.argv ?? [])
+  const isEmptyArgs = !Object.keys(args).length
 
   if (args.h) {
     printHelp()
   }
 
   if (args.s) {
-    // Сохранить город
+    await saveCity(args.s)
   }
 
   if (args.t) {
-    return saveToken(args.t)
+    await saveToken(args.t)
   }
 
-  await getWeather('Saransk')
+  if (isEmptyArgs) {
+    return getForecast()
+  }
 }
 
 initCLI()
